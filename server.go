d5028b6e0fab180075d9405b41059f9eae59046b
Unk0wn <73479844+asherlr23@users.noreply.github.com>
2023-05-22 17:24:52 -0400

    Update server.go

--- server.go
+++ server.go
@@ -1,12 +1,23 @@
 package main
 
 type Server struct {
-	EnableLogs           bool
-	IDCounter            int
-	IPBlocklist          []string
+	// Other
+	EnableLogs  bool
+	IDCounter   int
+	IPBlocklist []string
+
+	// Config
+	ServerVersion        string
 	RejectClients        bool
 	EnableScratchSupport bool
 	CheckIPAddresses     bool
-	EnableMotd           bool
-	MotdMessage          string
+	EnableMOTD           bool
+	MOTDMessage          string
+
+	// Managing methods
+	CustomMethods     func()
+	DisabledMethods   map[string]struct{}
+	MethodCallbacks   map[string]interface{}
+	ListenerCallbacks map[string]interface{}
+	SafeMethods       map[string]struct{}
 }
